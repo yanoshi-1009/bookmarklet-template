34a18d899e2cd5bc6ec5171e26be8061ef086a53
yanoshi-1009 <y.idakko@gmail.com>
2024-08-21 17:27:36 +0900

    Add frequently used functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 (async () => {
   "use strict";
 
-  // URLチェック
-  if (
-    !/^https:\/\/.+\.(kintone.com|cybozu.com|cybozu.cn)\/k\//.test(
-      location.href
-    )
-  ) {
-    window.alert("This bookmarklet does not work on this site.");
-    return;
-  }
+  const checkURL = () => {
+    if (
+      !/^https:\/\/.+\.(kintone.com|cybozu.com|cybozu.cn)\/k\//.test(
+        location.href
+      )
+    ) {
+      window.alert("This bookmarklet does not work on this site.");
+      return false;
+    }
+    return true;
+  };
+
+  const loadCDN = async (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve();
+      };
+      document.head.appendChild(script);
+    });
+  };
+
+  const downloadFile = (blob, fileName, fileExtension) => {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = `${fileName}.${fileExtension}`;
+    a.click();
+  };
+
+  // Main
+  if (!checkURL()) return;
+
+  location.reload();
 })();
